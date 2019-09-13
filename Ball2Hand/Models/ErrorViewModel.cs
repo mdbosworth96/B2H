00ecd3c902b40e135bc22b60e5a906ea0442bc3b
Matt <mdbosworth96@hotmail.com>
2019-09-13 21:09:49 +0100

    first commit

---
+++ Ball2Hand/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ball2Hand.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
